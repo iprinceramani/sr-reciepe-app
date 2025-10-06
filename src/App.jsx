a0f882a7001a29cc43e792484e713ce9e19f9372
Prince Ramani <iprinceramani@gmail.com>
2025-10-06 16:25:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Route, Routes } from 'react-router'
+import Cuisines from './components/cuisines'
+import '@tailwindplus/elements';
+import Navbar from './components/Navbar'
+import Receipies from './components/Receipies'
+import FullReceipe from './components/FullReceipie'
+import Meals from './components/Meals'
+function App() {
+ 
+  return (
+    <>
+    <Navbar></Navbar>
+    <Routes>
+        <Route path='/' element={<Cuisines></Cuisines>}></Route>
+      <Route path='/Cuisines' element={<Cuisines></Cuisines>}></Route>
+      <Route path="/Receipies/:cuisine" element={<Receipies></Receipies>}></Route>
+      <Route path="/Receipies" element={<Receipies></Receipies>}></Route>
+      <Route path='/Fullreceipe/:id' element={<FullReceipe></FullReceipe>}></Route>
+      <Route path='/Meals/:meal' element={<Meals></Meals>}></Route>
+    </Routes>
+    </>
+  )
+}
+
+export default App
